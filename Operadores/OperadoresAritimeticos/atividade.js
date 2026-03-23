dcd102eceaae6f28d5559d2977269720f55db0ff
EDUARDA DUANE DOS SANTOS SENNA <04534658036@senacrs.edu.br>
2026-03-23 17:14:28 -0300

    Operadores

---
+++ Operadores/OperadoresAritimeticos/atividade.js
@@ -0,0 +1,27 @@
+//-----------ATIVIDADE 1--------------------------------------------
+const sexta = 2
+const sabado = 5
+
+const resultado = sexta + sabado + 3
+
+console.log(resultado)
+
+
+//-----------ATIVIDADE 2---------------------------------------------
+
+const hamburguer=15
+const batata = 10
+
+const resultado2 = hamburguer + batata +15
+
+console.log(resultado2)
+
+//-----------ATIVIDADE 3--------------------------------------------
+
+const fone= 120
+const parcelas= 3
+
+const resultado3 = fone / parcelas
+
+console.log(resultado3)
+
---
+++ Operadores/OperadoresAritimeticos/conteudo.js
@@ -0,0 +1,62 @@
+//soma
+const primeiroValor= 10;
+const segundoValor =20
+
+const resultado = primeiroValor + segundoValor
+console.log(resultado)
+
+//subtração
+const primeiroValor2= 30
+const segundoValor2 =5
+
+const resultado2 = primeiroValor2 - segundoValor2 +444
+console.log(resultado2)
+
+//multiplicação
+const primeiroValor3 = 10
+const segundoValor3 = 25
+const resultado3= primeiroValor3 * segundoValor3
+
+console.log(resultado3)
+
+//divisão
+const primeiroValor4= 345
+const segundoValor4=10
+
+const resultado4= primeiroValor4 / segundoValor4
+console.log(resultado4)
+
+//resto da divisão
+const restoDaDivisão = 11 % 4
+console.log(restoDaDivisão)
+
+//Comparadores
+//Igual: ===
+//Diferente: !==
+//Maior e maior igual = > e >=
+//Menor e menor igual: < e <=
+
+//=== verifica se o valor e o tipo são iguais
+"1" === "2" // false, são diferentes
+"2" === "2" //true, são iguais
+ 2 === "2" // false, são tipo diferente
+
+ const condição = 1 === 2 
+ // o valor sai da comparação
+ // pode ser guardado em uma variável
+ // nesse caso, condição === false
+ 
+ "1" !== "2"//true, são diferentes
+ "2" !== "2" //false, são iguais
+ 2 !== "2" //true, tipos diferentes
+
+ const condicao = 1 !== 2 //true
+
+ 1 > 2 // false, porque 1 é menor que 2
+ 2 > 2 // false, porque 2 é igual a 2
+ 3 > 2 //true, porque 3 é maior que 2
+
+ 1 >= 2  // false, porque 1 é menor que 2
+ 2 >= 2 // true, porque 2 é igual a 2
+ 3>= 2 // true, porque 3 é maior que 2
+ 
