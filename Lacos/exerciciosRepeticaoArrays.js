1f1cb18c15ee867ec99c7d00e739d2f4343578ee
EDUARDA DUANE DOS SANTOS SENNA <04534658036@senacrs.edu.br>
2026-04-28 16:52:58 -0300

    Subindo arquivos

--- Lacos/exerciciosRepeticaoArrays.js
+++ Lacos/exerciciosRepeticaoArrays.js
@@ -21,11 +21,39 @@ while(i < moedas.length){
 }
 console.log(`Você tem ${totalMoedas} moedas cara!`)*/
 
-let alienígenas = ["José" , "Maria" , "Marcos" , "Matheus" , "Fransisco" , "Ana" , "Sofia" , "Enzo" , "Valentina"]  
+/*let alienígenas = ["José" , "Maria" , "Marcos" , "Matheus" , "Fransisco" , "Ana" , "Sofia" , "Enzo" , "Valentina"]  
 
 let i = 0 
 
 while(i < alienígenas.length){
     console.log(`Oi ${alienígenas[i]} , vem dançar!`)
     i++
+}*/
+
+/*let numeros = [1, 5, 7, 9, 12];
+
+let i = 0;
+let maiorNumero = numeros[0]; 
+
+while(i < numeros.length) {    
+    if (numeros[i] > maiorNumero) {
+        maiorNumero = numeros[i];
+    }
+    
+    i++;
 }
+console.log(`O verdadeiro tesouro  foi o número ${maiorNumero}!`);*/
+
+
+let velocidades = [5, 10 ,220, 30 ,12, 1]
+let i = 0;
+let valor = velocidades[0]
+
+while(i < velocidades.length){
+    if(velocidades[i] > valor){
+        valor = velocidades[i]
+    }
+    i++
+}
+console.log(`O corredor mais rápido atingiu ${valor} Km/h!!`)
+
---
+++ Lacos/for.js
@@ -0,0 +1,9 @@
+/*for (let i = 0; i < 5; i++){
+    console.log("Repetição numero: ", i)
+}*/
+
+let numeros = [14, 45, 10, 56, 15]
+for(let i = 0; i < 5; i++){
+    let elemento = numeros[i]
+    console.log(elemento)
+}
